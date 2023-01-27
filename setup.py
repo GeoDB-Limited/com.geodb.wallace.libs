afaff969d49221f2f8248b5dcc79d1108a33aaa0
AlbaranezJavier <32954090+AlbaranezJavier@users.noreply.github.com>
2023-01-27 12:17:52 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+PACKAGE_NAME = 'wallace_libs'
+PACKAGE_DESCRIPTION = 'wallace_libs'
+PACKAGE_VERSION = '0.0.1'
+setuptools.setup(
+    name=PACKAGE_NAME,
+    version=PACKAGE_VERSION,
+    description=PACKAGE_DESCRIPTION,
+    install_requires=[],
+    packages=setuptools.find_packages(),
+)

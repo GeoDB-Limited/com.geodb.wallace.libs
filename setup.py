270f8a79146c309b3009980ff4c42792a1b5ff52
AlbaranezJavier <32954090+AlbaranezJavier@users.noreply.github.com>
2023-02-03 09:55:12 +0100

    new_version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='wallace_libs',
-    version='0.0.1',
+    version='0.0.2',
     description='wallace_libs',
     install_requires=[
         'requests==2.28.1',

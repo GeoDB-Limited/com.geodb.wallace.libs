8fe3bb1bb1c8a868483be8b43302a8d3e7d96d85
AlbaranezJavier <32954090+AlbaranezJavier@users.noreply.github.com>
2023-02-03 09:48:23 +0100

    add install requires

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setuptools.setup(
     name='wallace_libs',
     version='0.0.1',
     description='wallace_libs',
-    install_requires=[],
+    install_requires=[
+        'requests==2.28.1'
+        'google-cloud-core==1.6.0',
+    ],
     packages=["wallace_libs"],
 )

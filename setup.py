c7191c271e91299c88605ceb39f2686d0dd1feea
AlbaranezJavier <32954090+AlbaranezJavier@users.noreply.github.com>
2023-02-03 09:50:14 +0100

    changes

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setuptools.setup(
     version='0.0.1',
     description='wallace_libs',
     install_requires=[
-        'requests==2.28.1'
-        'google-cloud-core==1.6.0',
+        'requests==2.28.1',
+        'google-cloud-core==1.6.0'
     ],
     packages=["wallace_libs"],
 )
